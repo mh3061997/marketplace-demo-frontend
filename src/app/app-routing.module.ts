309a32a253eb331e1ac5d63f6649c32617ea99bc
mohamed.haitham <mohamed.haitham@systemsltd.com>
2023-02-07 11:36:30 +0200

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { MarketPageComponent } from './components/market-page/market-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'market',
+    component: MarketPageComponent,
+    // canActivate:LoginGuard
+  },
+  {
+    path: '**',
+    component: LoginPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
