8077b0d3f326776778a84949fed2344b22c31056
mohamed.haitham <mohamed.haitham@systemsltd.com>
2023-02-07 11:28:58 +0200

    add skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { MarketPageComponent } from './components/market-page/market-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotificationListComponent } from './components/notification-list/notification-list.component';
+import { ItemComponent } from './components/market/item/item.component';
+import { LimitedQuantityPipePipe } from './pipes/limited-quantity-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MarketPageComponent,
+    NavbarComponent,
+    NotificationListComponent,
+    ItemComponent,
+    LimitedQuantityPipePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/limited-quantity-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LimitedQuantityPipePipe } from './limited-quantity-pipe.pipe';
+
+describe('LimitedQuantityPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new LimitedQuantityPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
